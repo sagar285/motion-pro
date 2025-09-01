d77433a233771368454cf87055157a618c6a9cff
sagar285 <anandguptasir@gmail.com>
2025-09-01 10:49:39 +0530

    Eslint improved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,3 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-//   {
-//     ignores: [
-//       "node_modules/**",
-//       ".next/**",
-//       "out/**",
-//       "build/**",
-//       "next-env.d.ts",
-//     ],
-//   },
-// ];
-
-// export default eslintConfig;
 import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
@@ -40,6 +15,38 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      // Fix unused variables warnings - allow underscore prefix
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_"
+      }],
+      
+      // Turn off base rule to avoid conflicts
+      "no-unused-vars": "off",
+      
+      // Fix explicit any warnings - downgrade to warning
+      "@typescript-eslint/no-explicit-any": "warn",
+      
+      // Fix other common TypeScript warnings
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      
+      // Allow unescaped entities in JSX (quotes and apostrophes)
+      "react/no-unescaped-entities": ["error", {
+        "forbid": [">", "}"]
+      }],
+      
+      // Relax some React hooks rules
+      "react-hooks/exhaustive-deps": "warn",
+      
+      // Allow img tags (or make it a warning)
+      "@next/next/no-img-element": "warn"
+    }
+  }
 ];
 
 export default eslintConfig;
